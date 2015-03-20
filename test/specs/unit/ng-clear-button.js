bd2414eee67f49ab6162f3fad712353cd7b3b4f9
Tamer Aydin <tamer.aydin@gmail.com>
2015-03-20 17:43:52 +0100

    added unit tests for controller

--- test/specs/unit/ng-clear-button.js
+++ test/specs/unit/ng-clear-button.js
@@ -8,9 +8,68 @@ describe('ngClearButton', function() {
     scope,
     rootScope,
     controller,
-    element;
+    button,
+    element,
+    ClearButtonOptions,
+    ClearButtonClassNames;
 
   describe('ClearButtonController', function() {
+
+    beforeEach(function() {
+      inject(function($injector, _$rootScope_, _$controller_) {
+        ClearButtonOptions = $injector.get('ClearButtonOptions');
+        ClearButtonClassNames = $injector.get('ClearButtonClassNames');
+        rootScope = _$rootScope_;
+        controller = _$controller_;
+        scope = _$rootScope_.$new();
+
+        controller('ClearButtonController', {
+          $scope: scope,
+          $rootScope: rootScope,
+          ClearButtonOptions: ClearButtonOptions,
+          ClearButtonClassNames: ClearButtonClassNames
+        });
+      });
+
+      button = angular.element('<span>button</span>');
+    });
+
+    it('initial values should be set', function() {
+      expect(scope.isButtonVisible).toBeFalsy();
+    });
+
+    it('onMouseDown should work', function() {
+      ClearButtonOptions.isVisible = true;
+
+      scope.onMouseDown(button);
+      expect(scope.isButtonVisible).toBeTruthy();
+
+      ClearButtonOptions.isVisible = false;
+      scope.onMouseDown(button);
+      expect(scope.isButtonVisible).toBeFalsy();
+
+      button.addClass(ClearButtonClassNames.FOCUSED_INPUT_BUTTON);
+      scope.onMouseDown(button);
+      expect(scope.isButtonVisible).toBeTruthy();
+    });
+
+    it('onMouseUp should work', function() {
+      // TODO
+    });
+
+    it('onInputFocus should work', function() {
+      scope.onInputFocus(button);
+      expect(button.hasClass(ClearButtonClassNames.FOCUSED_INPUT_BUTTON)).toBeTruthy();
+    });
+
+    it('onInputBlur should work', function() {
+      scope.onInputFocus(button);
+      scope.onInputBlur(button);
+      expect(button.hasClass(ClearButtonClassNames.FOCUSED_INPUT_BUTTON)).toBeFalsy();
+    });
+  });
+
+  describe('withClearButton', function() {
 
     beforeEach(inject(function(_$compile_, _$rootScope_, _$controller_) {
       compiler = _$compile_;
@@ -24,12 +83,13 @@ describe('ngClearButton', function() {
       });
 
       element = compiler(
-        '<input type="search" with-clear-button />')(scope);
+        '<div><input type="search" with-clear-button /></div>')(scope);
     }));
 
-    // TODO: add unit tests
+    // TODO
     it('should initialize properly', function() {
-      expect(true).toBe(true);
+      expect(element[0].innerHTML).toContain('ng-clear-button__input');
+      expect(element[0].innerHTML).toContain('ng-clear-button__button');
     });
   });
 });
