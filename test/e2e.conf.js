0b6603a11b914991f9850c0f2acbe49e5ef389fd
Tamer Aydin <tamer.aydin@gmail.com>
2015-03-21 12:04:19 +0100

    updated selenium commands for travis

--- test/e2e.conf.js
+++ test/e2e.conf.js
@@ -2,14 +2,6 @@ exports.config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
   specs: ['specs/e2e/ng-clear-button.js'],
   capabilities: {
-    'browserName': 'chrome',
-    'chromeOptions': {
-      'args': [
-        'no-sandbox',
-        'no-default-browser-check',
-        'no-first-run',
-        'disable-default-apps'
-      ]
-    }
+    'browserName': 'firefox'
   }
 };
